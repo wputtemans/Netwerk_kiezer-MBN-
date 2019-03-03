b7da846d71d7acc4f06454b65a2a934ba864d5d0
wputtemans <47752914+wputtemans@users.noreply.github.com>
2019-03-03 12:37:08 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CSharp_SMS
+{
+    partial class Form_SMS_Sender
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(21, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(146, 29);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.Text = "B Mobistar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(217, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(119, 29);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "NL KPN";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(217, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(160, 29);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.Text = "NL T-Mobile";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Location = new System.Drawing.Point(16, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(385, 58);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "geen AT Poort gevonden";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(21, 74);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(152, 29);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.Text = "B Proximus";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Singaalsterkte: ";
+            // 
+            // Form_SMS_Sender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 230);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form_SMS_Sender";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Netwerk kiezer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Management;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MbnApi;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace CSharp_SMS
+{
+    public partial class Form_SMS_Sender : Form
+    {
+        private IMbnInterfaceManager m_MbnInterfaceManager;
+        private IMbnConnectionManager m_MbnConnectionManager;
+        private IMbnInterface m_MbnInterface;
+        private IMbnConnection m_MbnConnection;
+        private IMbnDeviceServicesContext m_MbnDeviceServicesContext;
+        private IMbnDeviceServicesManager m_MbnDeviceServicesManager;
+        private IMbnInterface inf;
+        System.Timers.Timer t = new System.Timers.Timer(10000);
+
+        public Form_SMS_Sender()
+        {
+            InitializeComponent();
+            InitializeManagers();
+            this.TopMost = true;
+            t.AutoReset = true;
+            t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+            t.Start();
+
+            MbnInterfaceManager mbnInfMgr = new MbnInterfaceManager();
+            IMbnInterfaceManager infMgr = (IMbnInterfaceManager)mbnInfMgr;
+            IMbnInterface[] interfaces = (IMbnInterface[])infMgr.GetInterfaces();
+            inf = interfaces[0];
+            MBN_INTERFACE_CAPS infcap = inf.GetInterfaceCapability();
+            IMbnRegistration registrationInterface = m_MbnInterface as IMbnRegistration;
+            label1.Text = ((infcap.manufacturer + " - " + infcap.model).ToString());
+        }
+
+        private void aanpassen(object sender, EventArgs e)
+        {
+            Thread.Sleep(1000);
+
+            if (radioButton1.Checked)
+            {              
+                IMbnRegistration registrationInterface = m_MbnInterfaceManager.GetInterface(inf.InterfaceID) as IMbnRegistration;
+                uint requestId;
+                registrationInterface.SetRegisterMode(MBN_REGISTER_MODE.MBN_REGISTER_MODE_MANUAL, "20610", UInt32.Parse("2147483708"), out requestId);
+            }
+
+            else if (radioButton2.Checked)
+            {
+                IMbnRegistration registrationInterface = m_MbnInterfaceManager.GetInterface(inf.InterfaceID) as IMbnRegistration;
+                uint requestId;
+                registrationInterface.SetRegisterMode(MBN_REGISTER_MODE.MBN_REGISTER_MODE_MANUAL, "20408", UInt32.Parse("2147483708"), out requestId);
+            }
+            else if (radioButton3.Checked)
+            {
+                IMbnRegistration registrationInterface = m_MbnInterfaceManager.GetInterface(inf.InterfaceID) as IMbnRegistration;
+                uint requestId;
+                registrationInterface.SetRegisterMode(MBN_REGISTER_MODE.MBN_REGISTER_MODE_MANUAL, "20416", UInt32.Parse("2147483708"), out requestId);
+            }
+            else if (radioButton4.Checked)
+            {
+                IMbnRegistration registrationInterface = m_MbnInterfaceManager.GetInterface(inf.InterfaceID) as IMbnRegistration;
+                uint requestId;
+                registrationInterface.SetRegisterMode(MBN_REGISTER_MODE.MBN_REGISTER_MODE_MANUAL, "20601", UInt32.Parse("2147483708"), out requestId);
+            }
+            Thread.Sleep(1000);
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            aanpassen(3,e);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            aanpassen(2, e);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            aanpassen(1, e);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            aanpassen(4, e);
+        }
+        public void InitializeManagers()
+        {
+            try
+            {
+                if (m_MbnInterfaceManager == null)
+                {
+                    m_MbnInterfaceManager = (IMbnInterfaceManager)new MbnInterfaceManager();
+                }
+
+                if (m_MbnConnectionManager == null)
+                {
+                    m_MbnConnectionManager = (IMbnConnectionManager)new MbnConnectionManager();
+                }
+
+                if (m_MbnDeviceServicesManager == null)
+                {
+                    m_MbnDeviceServicesManager = (IMbnDeviceServicesManager)new MbnDeviceServicesManager();
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+        private void t_Elapsed(object sender,System.Timers.ElapsedEventArgs e)
+        {
+            //IMbnSignal sig = (IMbnSignal)inf;
+            //uint signalStrength = sig.GetSignalStrength();
+            //label2.Invoke((Action)delegate
+            //{ label2.Text = "Signaalsterkte: " + signalStrength; });
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "netsh.exe";
+            cmd.StartInfo.Arguments = "mbn show signal interface=\"mobiel*\"";
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.Start();
+            string output = cmd.StandardOutput.ReadToEnd();
+            output = output.Substring(output.IndexOf("%") - 2, 3);
+            label2.Invoke((Action)delegate
+            { label2.Text = "Signaalsterkte: " + output; });
+            cmd.WaitForExit();
+        }
+    }
+}
